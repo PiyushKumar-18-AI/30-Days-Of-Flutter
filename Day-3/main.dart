98b03e5704a0f60280d50ed340fc0d5bcf64461f
Piyush Kumar <68659006+PiyushKumar-18-AI@users.noreply.github.com>
2023-01-25 00:06:42 +0530

    main.dart updated

---
+++ Day-3/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_tutorial/screens/home_page.dart';
+import 'package:flutter_tutorial/screens/login_page.dart';
+
+void main() {
+  // ignore: prefer_const_constructors
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      // initialRoute: "/home",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+      },
+    );
+  }
+}
