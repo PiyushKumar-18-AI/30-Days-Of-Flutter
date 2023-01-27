001b202bd78e9a76ed883369f47958c2440a1465
Piyush Kumar <68659006+PiyushKumar-18-AI@users.noreply.github.com>
2023-01-27 22:57:16 +0530

    Initial Route Changed to Home Page

---
+++ Day-9/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_tutorial/screens/home_page.dart';
+import 'package:flutter_tutorial/screens/login_page.dart';
+import 'package:flutter_tutorial/utils/routes.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  // ignore: prefer_const_constructors
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+          primarySwatch: Colors.deepPurple,
+          fontFamily: GoogleFonts.duruSans().fontFamily),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      initialRoute: MyRoutes.homeRoute,
+      routes: {
+        "/": (context) => LoginPage(),
+        MyRoutes.homeRoute: (context) => HomePage(),
+        MyRoutes.loginRoute: (context) => LoginPage(),
+      },
+    );
+  }
+}
+
+// In this Main.dart file I changed the initialRoute of the app since we would be working on the Home Page now.
