be0c3c49d5acefda27b371ff911cb4935f37b11e
Piyush Kumar <68659006+PiyushKumar-18-AI@users.noreply.github.com>
2023-01-28 23:56:36 +0530

    AppBar Updated

---
+++ Day-10/screens/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_tutorial/widgets/drawer.dart';
+
+class HomePage extends StatelessWidget {
+  final int days = 30;
+  final String name = "PK";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+      body: Center(
+          child: Center(
+        child:
+            Container(child: Text("Welcome to $days Days of Flutter by $name")),
+      )),
+      drawer: MyDrawer(),
+    );
+  }
+}
