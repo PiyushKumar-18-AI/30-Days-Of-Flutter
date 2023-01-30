dabd44abb3fb2302d8b2fc58c6ba873bad521a5e
Piyush Kumar <68659006+PiyushKumar-18-AI@users.noreply.github.com>
2023-01-31 03:20:17 +0530

    CircularProgressIndicator, and Loaded JSON added

--- Day-13 to 15/screens/home_page.dart
+++ Day-13 to 15/screens/home_page.dart
@@ -9,6 +9,7 @@ class HomePage extends StatefulWidget {
   @override
   State<HomePage> createState() => _HomePageState();
 }
+
 class _HomePageState extends State<HomePage> {
   final int days = 30;
 
@@ -19,6 +20,7 @@ class _HomePageState extends State<HomePage> {
     super.initState();
     loadData();
   }
+  
   loadData() async {
     await Future.delayed(Duration(seconds: 2));
     final catalogJson =
@@ -30,6 +32,7 @@ class _HomePageState extends State<HomePage> {
         .toList();
     setState(() {});
   }
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
