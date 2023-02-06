df653338c235e3bb2e5dc3fbe2551de6aff53b39
Piyush Kumar <68659006+PiyushKumar-18-AI@users.noreply.github.com>
2023-02-06 22:03:33 +0530

    Home Page Updated

---
+++ Day-18/lib/screens/home_page.dart
@@ -0,0 +1,61 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_tutorial/models/catalog.dart';
+import 'package:flutter_tutorial/widgets/home_widgets/catalog_header.dart';
+import 'package:flutter_tutorial/widgets/home_widgets/catalog_list.dart';
+import 'package:flutter_tutorial/widgets/drawer.dart';
+import 'package:flutter_tutorial/widgets/item_widget.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+import '../widgets/themes.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final int days = 30;
+
+  final String name = "PK";
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    await Future.delayed(Duration(seconds: 2));
+    final catalogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    final decodedData = jsonDecode(catalogJson);
+    var productsData = decodedData["products"];
+    CatalogModel.items = List.from(productsData)
+        .map<Item>((item) => Item.fromMap(item))
+        .toList();
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: MyTheme.creamColor,
+        body: SafeArea(
+          child: Container(
+            padding: Vx.m32,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CatalogHeader(),
+                if (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
+                  CatalogList().py16().expand()
+                else
+                  CircularProgressIndicator().centered().expand(),
+              ],
+            ),
+          ),
+        ));
+  }
+}
