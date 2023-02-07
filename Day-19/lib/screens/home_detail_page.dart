1fa352f25594a307e7c77026ae925c8fb155a163
Piyush Kumar <68659006+PiyushKumar-18-AI@users.noreply.github.com>
2023-02-07 23:15:00 +0530

    Dummy Text Added and AppBar Updated

---
+++ Day-19/lib/screens/home_detail_page.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_tutorial/models/catalog.dart';
+import 'package:flutter_tutorial/widgets/themes.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+class HomeDetailPage extends StatelessWidget {
+  final Item catalog;
+
+  const HomeDetailPage({Key? key, required this.catalog})
+      : assert(catalog != null),
+        super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: MyTheme.creamColor,
+        elevation: 0,
+      ),
+      backgroundColor: MyTheme.creamColor,
+      bottomNavigationBar: Container(
+        color: Colors.white,
+        child: ButtonBar(
+          alignment: MainAxisAlignment.spaceBetween,
+          buttonPadding: EdgeInsets.zero,
+          children: [
+            "\$${catalog.price}".text.bold.xl4.red800.make(),
+            ElevatedButton(
+              onPressed: () {},
+              style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all(MyTheme.darkBluishColor),
+                  shape: MaterialStateProperty.all(StadiumBorder())),
+              child: "Add to cart".text.make(),
+            ).wh(120, 50)
+          ],
+        ).p32(),
+      ),
+      body: SafeArea(
+        bottom: false,
+        child: Column(
+          children: [
+            Hero(
+              tag: Key(catalog.id.toString()),
+              child: Image.network(catalog.image),
+            ).h32(context),
+            Expanded(
+                child: VxArc(
+              height: 30.0,
+              arcType: VxArcType.CONVEY,
+              edge: VxEdge.TOP,
+              child: Container(
+                color: Colors.white,
+                width: context.screenWidth,
+                child: Column(
+                  children: [
+                    catalog.name.text.xl4
+                        .color(MyTheme.darkBluishColor)
+                        .bold
+                        .make(),
+                    catalog.desc.text.textStyle(context.captionStyle).xl.make(),
+                    10.heightBox,
+                    "Sanctus no rebum stet nonumy labore sanctus, at sanctus justo kasd at. Ipsum diam sit eirmod consetetur ea clita rebum, sed stet consetetur et sed takimata voluptua. No eirmod duo rebum duo est. Vero elitr et takimata et ipsum, consetetur amet duo amet aliquyam accusam at vero accusam lorem. Justo eos sanctus sed est amet magna amet ut. Magna ea elitr diam elitr et nonumy aliquyam gubergren, nonumy at no sanctus accusam et tempor, elitr accusam vero labore dolor, ipsum."
+                        .text
+                        .textStyle(context.captionStyle)
+                        .make()
+                        .p16()
+                  ],
+                ).py64(),
+              ),
+            ))
+          ],
+        ),
+      ),
+    );
+  }
+}
