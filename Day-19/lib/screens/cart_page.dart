e7b7daacef2f1b7b6e20ebd9db92732be4371b45
Piyush Kumar <68659006+PiyushKumar-18-AI@users.noreply.github.com>
2023-02-07 23:18:15 +0530

    New Cart Page Added

---
+++ Day-19/lib/screens/cart_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_tutorial/widgets/themes.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+class CartPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: MyTheme.creamColor,
+      appBar: AppBar(
+        title: "Cart".text.make(),
+      ),
+    );
+  }
+}
